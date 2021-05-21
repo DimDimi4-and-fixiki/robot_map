29b9e8d0ebfa2b3ded2cd3328e8d4089c8411f7e
DimDimi4 <dimdimi4andfixiki@gmail.com>
2021-05-21 17:07:40 +0300

    Map Updated

--- map/static/js/main.js
+++ map/static/js/main.js
@@ -47,13 +47,60 @@ function init () {
     var myMap = new ymaps.Map('map', {
         center: [55.78123982928414,37.73150623544307],
         zoom: 13,
-        controls: [buttonEditor]
+        controls: [buttonEditor],
+        behaviors: ["drag"],
     }, {
         buttonMaxWidth: 300
     });
 
     // Добавляем мультимаршрут на карту.
+    ZoomLayout = ymaps.templateLayoutFactory.createClass("<div>" +
+                "<div id='zoom-in' class='btn-plus'><i class='icon-plus'></i></div><br>" +
+                "<div id='zoom-out' class='btn-minus'><i class='icon-minus'></i></div>" +
+                "</div>", {
+
+                /**
+                 * Redefining methods of the layout, in order to perform
+                 * additional steps when building and clearing the layout.
+                 */
+                build: function () {
+                    // Calling the "build" parent method.
+                    ZoomLayout.superclass.build.call(this);
+
+                    /**
+                     * Binding handler functions to the context and storing references
+                     * to them in order to unsubscribe from the event later.
+                     */
+                    this.zoomInCallback = ymaps.util.bind(this.zoomIn, this);
+                    this.zoomOutCallback = ymaps.util.bind(this.zoomOut, this);
+
+                    // Beginning to listen for clicks on the layout buttons.
+                    $('#zoom-in').bind('click', this.zoomInCallback);
+                    $('#zoom-out').bind('click', this.zoomOutCallback);
+                },
+
+                clear: function () {
+                    // Removing click handlers.
+                    $('#zoom-in').unbind('click', this.zoomInCallback);
+                    $('#zoom-out').unbind('click', this.zoomOutCallback);
+
+                    // Calling the "clear" parent method.
+                    ZoomLayout.superclass.clear.call(this);
+                },
+
+                zoomIn: function () {
+                    var map = this.getData().control.getMap();
+                    map.setZoom(map.getZoom() + 1, {checkZoomRange: true});
+                },
+
+                zoomOut: function () {
+                    var map = this.getData().control.getMap();
+                    map.setZoom(map.getZoom() - 1, {checkZoomRange: true});
+                }
+            }),
+            zoomControl = new ymaps.control.ZoomControl({options: {layout: ZoomLayout}});
     myMap.geoObjects.add(multiRoute);
+    myMap.controls.add(zoomControl);
 }
 
 ymaps.ready(init);
--- map/views.py
+++ map/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 
 mapbox_access_token = "pk.eyJ1IjoiZGltZGltaTQiLCJhIjoiY2tveDVsa2IzMGNuNjJvbzI2YWIzcmlmYyJ9.ypDiQfV9wTxGr7cXsE9_Mw"
 
+
 def index(request):
     return render(request, 'map/index.html', {'mapbox_access_token': mapbox_access_token})
 
